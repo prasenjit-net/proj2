1fab8c290bb2252962ec6c22026cbb188b0c6609
prasenjit-net <github@prasenjit.net>
2017-11-08 16:45:22 -0600

    test

--- src/app/blog-post/blog-post.component.ts
+++ src/app/blog-post/blog-post.component.ts
@@ -15,24 +15,40 @@ import 'rxjs/Rx';
 export class BlogPostComponent implements OnInit {
   md: string;
   blogData: any;
+  allBlogData: Array<Object>;
 
   constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.http.get("assets/posts.json")
-      .mergeMap(x => Observable.from(x as Array<Object>))
-      .filter(e => {
-        var link = this.route.snapshot.paramMap.get("link");
-        var r = e["link"] === link;
-        return r;
-      })
-      .subscribe(data => {
-        this.blogData = data;
-        this.http.get(`assets/${data["link"]}.md`, {responseType: 'text'})
-        .subscribe(mdText => {
-          this.md = mdText;
-        });
+    this.http.get("assets/posts.json").subscribe(data => {
+      this.allBlogData = data as Array<Object>;
+      this.route.paramMap.subscribe(pm => {
+        var link = pm.get("link");
+        for (var entry of this.allBlogData) {
+          if (entry["link"] === link) {
+            this.blogData = entry;
+            this.http.get(`assets/${entry["link"]}.md`, {responseType: 'text'})
+            .subscribe(mdText => {
+              this.md = mdText;
+            });
+          }
+        }
       });
+    });
+    // this.http.get("assets/posts.json")
+    //   .mergeMap(x => Observable.from(x as Array<Object>))
+    //   .filter(e => {
+    //     var link = this.route.snapshot.paramMap.get("link");
+    //     var r = e["link"] === link;
+    //     return r;
+    //   })
+    //   .subscribe(data => {
+    //     this.blogData = data;
+    //     this.http.get(`assets/${data["link"]}.md`, {responseType: 'text'})
+    //     .subscribe(mdText => {
+    //       this.md = mdText;
+    //     });
+    //   });
   }
 
 }
